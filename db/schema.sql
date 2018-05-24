a10a744b5c3275fee201c7c0caf37cc84ada41d2
Max Liao <max.liao@gmail.com>
2018-05-24 18:56:04 -0400

    JawsDB table

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
 ### Schema
-
-CREATE DATABASE burgers_db;
-
-USE burgers_db;
+USE wsxdz8gxloo7tjq1;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured boolean NOT NULL,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
